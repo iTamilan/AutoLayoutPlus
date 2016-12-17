1aa491a83fa16dd94fd6a1b6f3e34171a8d1957c
Tamilarasu <tamilarasu2608@gmail.com>
2016-12-17 18:07:47 +0530

    Initial

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Tamilarasu on 17/12/16.
+//  Copyright © 2016 Tamilarasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Tamilarasu on 17/12/16.
+//  Copyright © 2016 Tamilarasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
